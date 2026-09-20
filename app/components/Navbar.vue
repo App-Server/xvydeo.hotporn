3d940e5f23474053b53cfa3a934c0bd1f446fbec
Aragao <epicappcode@gmail.com>
2026-09-20 18:43:04 -0300

    first commit

---
+++ app/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-black border-bottom border-secondary-subtle" data-bs-theme="dark">
+            <div class="container">
+                <a class="navbar-brand fw-bold text-danger" href="#">Xvydeo Hot Porn</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Straight</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Gay</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Trans</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+
+    <Navbar />
+
+    <div class="container my-4">
+        <div class="row g-3 justify-content-center">
+            
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="ratio ratio-16x9">
+                    <iframe src="https://www.xvideos.com/embedframe/ikcphao5d17" frameborder="0" scrolling="no" allowfullscreen></iframe>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="ratio ratio-16x9">
+                    <iframe src="https://www.xvideos.com/embedframe/uhvfabd90c8" frameborder="0" scrolling="no" allowfullscreen></iframe>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="ratio ratio-16x9">
+                    <iframe src="https://www.xvideos.com/embedframe/otpkacf653f" frameborder="0" scrolling="no" allowfullscreen></iframe>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="ratio ratio-16x9">
+                    <iframe src="https://www.xvideos.com/embedframe/uptchchacac" frameborder="0" scrolling="no" allowfullscreen></iframe>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="ratio ratio-16x9">
+                    <iframe src="https://www.xvideos.com/embedframe/uplomod3234" frameborder="0" scrolling="no" allowfullscreen></iframe>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="ratio ratio-16x9">
+                    <iframe src="https://www.xvideos.com/embedframe/omomkcac0c1" frameborder="0" scrolling="no" allowfullscreen></iframe>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="ratio ratio-16x9">
+                    <iframe src="https://www.xvideos.com/embedframe/uledumhe6b1" frameborder="0" scrolling="no" allowfullscreen></iframe>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="ratio ratio-16x9">
+                    <iframe src="https://www.xvideos.com/embedframe/uatfuboae68" frameborder="0" scrolling="no" allowfullscreen></iframe>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="ratio ratio-16x9">
+                    <iframe src="https://www.xvideos.com/embedframe/okuukvb13e5" frameborder="0" scrolling="no" allowfullscreen></iframe>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</template>
