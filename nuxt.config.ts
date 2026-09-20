63d48635ed3858be8d86ca15afdb5a3270beb028
Aragao <epicappcode@gmail.com>
2026-09-20 19:11:48 -0300

    adsterra

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,21 @@ export default defineNuxtConfig({
           src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js',
           integrity: 'sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI',
           crossorigin: 'anonymous',
-          tagPosition: 'bodyClose' // Garante que o JS do Bootstrap carregue no final da página
+          tagPosition: 'bodyClose'
+        },
+        // Scripts globais da rede de anúncios
+        {
+          src: 'https://pl31436303.profitableratecpmnetwork.com/06/5b/21/065b214dfb2e80807ebeb7c6f9bc4b54.js',
+          defer: true
+        },
+        {
+          src: 'https://pl31436306.profitableratecpmnetwork.com/64/42/32/644232741aa0114c8ef020ea932f9987.js',
+          defer: true
+        },
+        {
+          src: 'https://pl31436304.profitableratecpmnetwork.com/fc937d79a666574dbebc15156cb96b43/invoke.js',
+          async: true,
+          'data-cfasync': 'false'
         }
       ]
     }
